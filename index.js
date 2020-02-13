5a376808822ff4571ddb2e6795a07575f19b1edd
CesarxArteaga <ctmces94@gmail.com>
2020-02-12 21:29:59 -0500

    m

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get('/', (req, res) =>{
     res.send("welcome bitch");
 } )
 
-app.listen(8000,function() {
+app.listen(process.env.PORT || 8000,function() {
     console.log('Listening on port 3000…')})
 
